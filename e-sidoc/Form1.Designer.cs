7435612453a931f95945bd2853c46ecc522adca9
tjaisson <thomas.jaisson@ac-paris.fr>
2019-12-03 17:54:23 +0100

    Ajout moulinette e-sidoc pour cité sco

---
+++ e-sidoc/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace e_sidoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB1
+            // 
+            this.TB1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TB1.Location = new System.Drawing.Point(12, 84);
+            this.TB1.Multiline = true;
+            this.TB1.Name = "TB1";
+            this.TB1.Size = new System.Drawing.Size(851, 387);
+            this.TB1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 483);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TB1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ e-sidoc/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace e_sidoc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string rootElemTag = "FICHES_XML";
+            const string nodePath = "/" + rootElemTag + "/EMPRUNTEURS";
+            const string idTag = "IDENTITE_ENT_M";
+            Dictionary<string, XmlNode> users = new Dictionary<string, XmlNode>();
+
+            void lit(XmlDocument xml)
+            {
+                foreach (XmlNode n in xml.SelectNodes(nodePath))
+                {
+                    var nn = n.SelectSingleNode(idTag);
+                    string id = nn.InnerText.Trim();
+                    if (!users.ContainsKey(id))
+                    {
+                        users.Add(id, n.Clone());
+                    }
+                }
+            }
+
+            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var xmlFile = new XmlDocument();
+            xmlFile.Load(basePath + @"\temp\Lavoisier\exportEsidoc eleves lycee.xml");
+            lit(xmlFile);
+            xmlFile = new XmlDocument();
+            xmlFile.Load(basePath + @"\temp\Lavoisier\exportEsidoc eleves clg.xml");
+            lit(xmlFile);
+
+            var xmlFileO = new XmlDocument();
+            var root = xmlFileO.CreateElement(rootElemTag);
+            foreach(XmlNode n in users.Values)
+            {
+                root.AppendChild(xmlFileO.ImportNode(n, true));
+            }
+
+            
+            xmlFileO.AppendChild(root);
+
+            var settings = new XmlWriterSettings();
+            settings.Encoding = Encoding.GetEncoding("windows-1252");
+            settings.Indent = true;
+            settings.IndentChars = "	";
+            using (var writer = XmlWriter.Create(basePath + @"\temp\Lavoisier\exportEsidoc eleves Lyc clg.xml", settings))
+            {
+                xmlFileO.Save(writer);
+            }
+
+        }
+    }
+}
